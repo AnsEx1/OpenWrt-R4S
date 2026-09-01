4a76d726e5a2d2dbbae56c81732895c41e3865ac
AnsEx <78890029+AnsEx1@users.noreply.github.com>
2026-09-01 13:12:13 +0800

    Add turboacc installation and UA3F package cloning
    Added script to install turboacc and clone UA3F package.

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,8 +3,9 @@
 # --- TTYD 免登录配置 ---
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
-# --- 添加 turboacc 加速插件 ---
+# --- 添加额外软件 ---
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+git clone https://github.com/SunBK201/UA3F.git package/UA3F
 
 # --- 版本号 ---
 fix_libgcc_version() {
