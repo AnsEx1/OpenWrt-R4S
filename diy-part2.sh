6aae856a52ee5414e44cfa7799ef3157ba13ad8d
AnsEx <78890029+AnsEx1@users.noreply.github.com>
2026-09-01 15:49:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,7 +3,7 @@
 # --- TTYD 免登录配置 ---
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
-# --- 添加额外软件 ---
+# --- 添加 turboacc 加速插件 ---
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # --- 版本号 ---
