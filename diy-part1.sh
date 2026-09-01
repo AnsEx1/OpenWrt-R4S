7a456452866388fff173409b799d4ea6a5587cec
AnsEx <78890029+AnsEx1@users.noreply.github.com>
2026-09-01 13:12:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,7 +4,3 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-
-#添加额外源
-echo "src-git UA3F https://github.com/SunBK201/UA3F.git" >> feeds.conf.default
-echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
