308c63cd39a84b823f592ae11a425ae0defd4948
AnsEx <78890029+AnsEx1@users.noreply.github.com>
2026-09-01 13:17:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,3 +4,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+
+# --- 添加额外软件 ---
+curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+git clone https://github.com/SunBK201/UA3F.git package/UA3F
+git clone https://github.com/JiaY-shi/fancontrol.git package/fancontrol
+git clone https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
