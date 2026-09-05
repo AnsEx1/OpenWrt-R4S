8f253b0fb876b579988ac669159b8dcf6ebd7871
AnsEx <78890029+AnsEx1@users.noreply.github.com>
2026-09-06 00:47:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,7 +4,7 @@
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # --- 添加 turboacc 加速插件 ---
-curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+# curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # --- 版本号 ---
 fix_libgcc_version() {
