4aafbafe8526cae50df26ef49b9c4cc026fb0a26
AnsEx <78890029+AnsEx1@users.noreply.github.com>
2026-09-07 16:16:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,6 +6,9 @@
 # Add a feed source
 
 # --- 添加额外软件 ---
-echo "src-git UA3F https://github.com/SunBK201/UA3F.git" >> feeds.conf.default
-echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
+# echo "src-git UA3F https://github.com/SunBK201/UA3F.git" >> feeds.conf.default
+# echo "src-git fancontrol https://github.com/JiaY-shi/fancontrol.git" >> feeds.conf.default
 # echo "src-git easytier https://github.com/EasyTier/luci-app-easytier.git" >> feeds.conf.default
+git clone https://github.com/SunBK201/UA3F.git package/UA3F
+git clone https://github.com/JiaY-shi/fancontrol.git package/fancontrol
+git clone https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier
